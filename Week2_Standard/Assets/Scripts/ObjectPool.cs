60b71377fc3a0d83bbc2bf632572c459039223f6
BaekSeungWoo617 <koke100@naver.com>
2024-10-21 12:58:49 +0900

    1

---
+++ Week2_Standard/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPool : MonoBehaviour
+{
+    public GameObject prefab;
+    GameObject[] monster;
+    private List<GameObject> pool = new List<GameObject>();
+    public int poolSize = 300;
+
+    void Start()
+    {
+        // 미리 poolSize만큼 게임오브젝트를 생성한다.
+        foreach(var pool in poolSize)
+        {
+            for (int i = 0; i < poolSize; i++)
+            {
+                List<GameObject> monster = new List<GameObject>();
+                monster = Instantiate(pool.prefab);
+                monster.SetActive(false);
+            }
+        }
+
+
+    }
+
+    public GameObject Get()
+    {
+        // 꺼져있는 게임오브젝트를 찾아 active한 상태로 변경하고 return 한다.
+    }
+
+    public void Release(GameObject obj)
+    {
+        // 게임오브젝트를 deactive한다.
+    }
+
+}
---
+++ Week2_Standard/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // [구현사항 1] 정적 필드 정의
+    private static QuestManager instance;
+
+    // [구현사항 2] 정적 프로퍼티 정의
+    private static QuestManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<QuestManager>();
+            }
+            if (instance == null)
+            {
+                instance = new GameObject().AddComponent<QuestManager>();// 나도 멀 한건지 모르겠다
+            }
+                return instance;
+        }
+    }
+
+    // [구현사항 3] 인스턴스 검사 로직
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            instance = this;
+        }
+    }
+}
